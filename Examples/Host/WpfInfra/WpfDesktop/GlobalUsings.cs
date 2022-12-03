7d6a035ce98b7d871b489ad7d3685b1b2826a4f6
kanadeiar <kanadeiar@gmail.com>
2022-12-03 18:38:12 +0300

    Исправления

--- Examples/Host/WpfInfra/WpfDesktop/GlobalUsings.cs
+++ Examples/Host/WpfInfra/WpfDesktop/GlobalUsings.cs
@@ -1,5 +1,6 @@
 global using Kanadeiar.Desktop.ViewModels;
 global using Kanadeiar.Desktop.ViewModels.Base;
+global using System.Windows.Input;
 global using Kanadeiar.Desktop.Commands;
 global using WpfDesktop.Abstracts;
 global using WpfDomain.Abstracts;
